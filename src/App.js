517a70d324871d8bf6166c7b1b69423c983ec2dd
RANUJ CHAUDHARY <tridevcomputer01@gmail.com>
2024-01-31 22:41:57 +0530

    tip index html update

--- src/App.js
+++ src/App.js
@@ -3,38 +3,9 @@
 import { useState } from "react";
 
 export default function App() {
-  const [bill, setBill] = useState("");
-  const [myTip, setMytip] = useState(0);
-  const [myFrndTip, setmyFrndTip] = useState(0);
-  const average = (bill * myTip + bill * myFrndTip) / 2;
   return (
     <div>
-      <div>
-        <span>How much was the bill?</span>
-        <input
-          value={bill}
-          onChange={(e) => setBill(parseFloat(e.target.value))}
-        />
-        {bill}
-      </div>
-      <div>
-        <span>How did you like the service?</span>
-        <SelectOptions onTip={setMytip} />
-        {myTip.toString().padEnd(4,'0')}
-      </div>
-      <div>
-        <span>How did your friend like the service?</span>
-        <SelectOptions onTip={setmyFrndTip} />
-        {myFrndTip.toString().padEnd(4,'0')}
-      </div>
-      <div>
-        <p>
-          <b>You Pay Rs</b>{" "}
-          {`${parseInt(bill) + Math.round(average)} (Rs.${bill} Rs.${Math.round(
-            average
-          )})`}
-        </p>
-      </div>
+      <p>this is react</p>
     </div>
   );
 }
