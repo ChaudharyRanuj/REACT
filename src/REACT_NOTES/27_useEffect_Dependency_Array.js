ba3b8bee3a65bd21a9ed49092958801aa6dbd785
RANUJ CHAUDHARY <tridevcomputer01@gmail.com>
2024-02-20 17:45:06 +0530

    added react notes folder

---
+++ src/REACT_NOTES/27_useEffect_Dependency_Array.js
@@ -0,0 +1,321 @@
+import { useEffect } from "react";
+import { Children, useState } from "react";
+
+/*
+Here is your key: 571f166
+
+Please append it to all of your API requests,
+
+OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=571f166
+
+How not to fetch data in react?
+
+useEffects to the Rescue
+
+Using Effects in Practice
+
+
+*/
+
+/*
+
+
+
+
+*/
+
+const tempMovieData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt0133093",
+    Title: "The Matrix",
+    Year: "1999",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt6751668",
+    Title: "Parasite",
+    Year: "2019",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+  },
+];
+
+const tempWatchedData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    runtime: 148,
+    imdbRating: 8.8,
+    userRating: 10,
+  },
+  {
+    imdbID: "tt0088763",
+    Title: "Back to the Future",
+    Year: "1985",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+    runtime: 116,
+    imdbRating: 8.5,
+    userRating: 9,
+  },
+];
+
+const average = (arr) =>
+  arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+
+export default function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [watched, setWatched] = useState(tempWatchedData);
+  const [query, setQuery] = useState("");
+
+  // useEffect with empty dependency Array
+  // FIRST APPROACH
+  // ***************
+  useEffect(function () {
+    console.log("After initial render");
+  }, []);
+
+  useEffect(function () {
+    console.log("B");
+  }, []);
+
+  console.log("C"); // will print first
+
+  // output C A B
+
+  // SECOND APPROACH
+// *****************
+
+  // WITH DEPENDENCY (ONLY MOUNTED ONCE)
+  useEffect(function () {
+    console.log("After initial render");
+  }, []);
+
+  // WITHOUT DEPENDENCY (THIS IS ALWAYS SYCHRONIZED WITH DATA)
+  useEffect(function () {
+    console.log("After each render");
+  });
+
+// WITH DEFINE STATE IN DEPENDENCY 
+// NOTE: render every time query state changes.
+  useEffect(function () {
+    console.log("After initial render");
+  }, [query]);
+
+
+  console.log("C"); // will print first
+
+  // output C B
+
+
+
+
+  // HANDLING FETCHING ERRORS
+  useEffect(
+    function () {
+      async function fetchMovie() {
+        try {
+          setIsLoading(true);
+          const res = await fetch(
+            `http://www.omdbapi.com/?i=tt3896198&apikey=571f166&s=${query}`
+          );
+
+          // HANDLING NETWORK ERROR HANDLING
+          if (!res.ok) {
+            throw new Error("Something went wrong with fetching movies.");
+          }
+
+          const data = await res.json();
+          // HANDLING API QUERY ERROR HANDLING
+          if (data.Response === "False") {
+            throw new Error("Movie not found.");
+          }
+          console.log(data);
+          setMovies(data.Search);
+        } catch (err) {
+          console.error(err.message);
+          setError(err.message);
+        } finally {
+          // used here to prevent duplication this is always run while execution of the code.
+          setIsLoading(false);
+        }
+      }
+      fetchMovie();
+    },
+    [query]
+  );
+
+  return (
+    <>
+      <NavBar>
+        <Search setQuery={setQuery} query={query} />
+        <NumResult movies={movies} />
+      </NavBar>
+      <MainMovieBox>
+        <Box>{isLoading ? <Loader /> : <MovieList movies={movies} />}</Box>
+        <Box>
+          <WatchedSummary watched={watched} />
+          <WatchList watched={watched} />
+        </Box>
+      </MainMovieBox>
+    </>
+  );
+}
+
+function Loader() {
+  return <p className="loader">Loading...</p>;
+}
+
+function NavBar({ children }) {
+  return (
+    <nav className="nav-bar">
+      <Logo />
+      {children}
+    </nav>
+  );
+}
+
+function NumResult({ movies }) {
+  return (
+    <p className="num-results">
+      Found <strong>{movies?.length}</strong> results
+    </p>
+  );
+}
+
+function Logo() {
+  return (
+    <div className="logo">
+      <span role="img">🍿</span>
+      <h1>usePopcorn</h1>
+    </div>
+  );
+}
+
+function Search({ setQuery, query }) {
+  return (
+    <input
+      className="search"
+      type="text"
+      placeholder="Search movies..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+    />
+  );
+}
+
+function MainMovieBox({ children }) {
+  return <main className="main">{children}</main>;
+}
+
+function Box({ children }) {
+  const [isOpen, setIsOpen] = useState(true);
+  return (
+    <div className="box">
+      <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
+        {isOpen ? "–" : "+"}
+      </button>
+      {isOpen && <>{children}</>}
+    </div>
+  );
+}
+
+function MovieList({ movies }) {
+  return (
+    <ul className="list">
+      {movies?.map((movie) => (
+        <Movie movie={movie} key={movie.imdbID} />
+      ))}
+    </ul>
+  );
+}
+
+function Movie({ movie }) {
+  return (
+    <li key={movie.imdbID}>
+      <img src={movie.Poster} alt={`${movie.Title} poster`} />
+      <h3>{movie.Title}</h3>
+      <div>
+        <p>
+          <span>🗓</span>
+          <span>{movie.Year}</span>
+        </p>
+      </div>
+    </li>
+  );
+}
+
+function WatchedSummary({ watched }) {
+  // DERIVED STATES
+  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
+  const avgUserRating = average(watched.map((movie) => movie.userRating));
+  const avgRuntime = average(watched.map((movie) => movie.runtime));
+  return (
+    <div className="summary">
+      <h2>Movies you watched</h2>
+      <div>
+        <p>
+          <span>#️⃣</span>
+          <span>{watched.length} movies</span>
+        </p>
+        <p>
+          <span>⭐️</span>
+          <span>{avgImdbRating}</span>
+        </p>
+        <p>
+          <span>🌟</span>
+          <span>{avgUserRating}</span>
+        </p>
+        <p>
+          <span>⏳</span>
+          <span>{avgRuntime} min</span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+function WatchList({ watched }) {
+  return (
+    <ul className="list">
+      {watched.map((movie) => (
+        <WatchedMovieList movie={movie} key={movie.imdbID} />
+      ))}
+    </ul>
+  );
+}
+
+function WatchedMovieList({ movie }) {
+  return (
+    <li key={movie.imdbID}>
+      <img src={movie.Poster} alt={`${movie.Title} poster`} />
+      <h3>{movie.Title}</h3>
+      <div>
+        <p>
+          <span>⭐️</span>
+          <span>{movie.imdbRating}</span>
+        </p>
+        <p>
+          <span>🌟</span>
+          <span>{movie.userRating}</span>
+        </p>
+        <p>
+          <span>⏳</span>
+          <span>{movie.runtime} min</span>
+        </p>
+      </div>
+    </li>
+  );
+}
---
+++ src/REACT_NOTES/3_styling_react_app.js
@@ -0,0 +1,28 @@
+/*
+WE CAN USE TO SYTYLE COMPONENTS
+
+INLINE CSS
+----------
+
+function Header() {
+
+  return (
+  **************  INLINE CSS ****************
+      <h1 style={{color: "red", fontSize: "48px", textTransform: "uppercase"}}> Fast React Pizza Co.</h1>
+   );
+}
+
+INTERNAL CSS
+------------
+
+EXTERNAL CSS
+SASS FILES
+CSS MODULES
+STYLE COMPONENTS
+TAILWIND 
+
+
+
+
+
+*/
