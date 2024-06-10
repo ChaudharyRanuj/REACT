11a77102d3ca474853a93e9ed3ae57bbdbf01f68
Ranuj Chaudhary <ranujchoudhary@gmail.com>
2024-06-10 15:05:29 +0530

    react steps to use action dispatch store reducer

--- src/REACT NOTES/REDUX/redux_state_management.js
+++ src/REACT NOTES/REDUX/redux_state_management.js
@@ -9,6 +9,7 @@ const reducer = (state = 5) => {
 // Redux methods are available from a Redux object
 // For example: Redux.createStore()
 // Define the store here:
+
 const store = Redux.createStore(reducer)
 
 
@@ -69,7 +70,189 @@ Another key principle in Redux is that state is read-only. In other words, the r
 The code editor has the previous example as well as the start of a reducer function for you. Fill in the body of the reducer function so that if it receives an action of type 'LOGIN' it returns a state object with login set to true. Otherwise, it returns the current state. Note that the current state and the dispatched action are passed to the reducer, so you can access the action's type directly with action.type.
 */
 
+// REDUCER
+// ***** REDUCER FOR COUNTER **********
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
 
-let arr = [1,2,3,4]
-let newArr = [...arr, 5]
-console.log(arr);
+const notesReducer = (state = 0, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
+    case DECREMENT:
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+// ***** REDUCER FOR AUTHENTICATION **********
+const LOGIN = "INCREMENT";
+const LOGOUT = "DECREMENT";
+
+const defaultState = {
+  authenticated: false,
+};
+
+const authReducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case LOGIN:
+      return {
+        authenticated: true,
+      };
+    case LOGOUT:
+      return {
+        authenticated: false,
+      };
+    default:
+      return state;
+  }
+};
+
+// COMBINE REDUCERS
+// Note: IF MORE REDUCER WERE USE WITH COMPOSING THE REDUCERS
+const rootReducer = Redux.cobineReducers({
+  auth: authReducer,
+  notes: notesReducer,
+});
+
+// Pass this rootRedcer to Store
+
+const store2 = Redux.createStore(rootReducer);
+
+// STORE
+
+// Redux methods are available from a Redux object
+// For example: Redux.createStore()
+// Define the store here:
+const store = Redux.createStore(authReducer);
+
+// ACTIONS
+const loginUser = () => {
+  return {
+    type: LOGIN,
+  };
+};
+
+const logoutUser = () => {
+  return {
+    type: LOGOUT,
+  };
+};
+
+//  ********** SEND ACTION DATA TO THE STORE ***********
+
+// We send action data with actions
+const addNoteText = (note) => {
+  return {
+    type: ADD_NOTE,
+    text: note,
+  };
+  // Note: text is the action data
+  // Change code above this line
+};
+
+// Store Listener
+let count = 0;
+function actionLog() {
+  return count++;
+}
+store.subscribe(actionLog);
+
+// DISPATCH
+store.dispatch(loginUser());
+store.dispatch(logoutUser());
+
+// ****************** Register a Store Listener ************************
+/*
+Another method you have access to on the Redux store object is store.subscribe().
+ This allows you to subscribe listener functions to the store, which are called
+  whenever an action is dispatched against the store. One simple use for this method
+   is to subscribe a function to your store that simply logs a message every time an
+    action is received and the store is updated.
+*/
+// *************** Use Cases for store.subscribe ******************
+/*
+1. Updating the UI
+One of the most common use cases for store.subscribe is to update the UI in response to changes in the application state. By subscribing to store updates, you can re-render your UI components with the latest state, ensuring that your application remains in sync with the latest data.
+
+2. Logging and Debugging
+store.subscribe can be used to log changes to the store's state, making it easier to debug and understand the flow of your application. By logging the updated state, you can identify issues and optimize your application's performance.
+
+3. Cache Management
+store.subscribe can be used to manage caches and ensure that stale data is not displayed to the user. By subscribing to store updates, you can invalidate caches and refresh data when the store's state changes.
+
+4. Analytics and Telemetry
+store.subscribe can be used to track user interactions and send analytics data to your backend. By subscribing to store updates, you can capture user behavior and send telemetry data to your analytics platform.
+
+*/
+
+// *************** Use Middleware to Handle Asynchronous Actions ************
+
+const REQUESTING_DATA = "REQUESTING_DATA";
+const RECEIVED_DATA = "RECEIVED_DATA";
+
+const requestingData = () => {
+  return { type: REQUESTING_DATA };
+};
+const receivedData = (data) => {
+  return { type: RECEIVED_DATA, users: data.users };
+};
+
+const handleAsync = () => {
+  return function (dispatch) {
+    // Dispatch request action here
+    dispatch(requestingData());
+    setTimeout(function () {
+      let data = {
+        users: ["Jeff", "William", "Alice"],
+      };
+      // Dispatch received data action here
+      dispatch(receivedData(data));
+    }, 2500);
+  };
+};
+
+const defaultState = {
+  fetching: false,
+  users: [],
+};
+
+const asyncDataReducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case REQUESTING_DATA:
+      return {
+        fetching: true,
+        users: [],
+      };
+    case RECEIVED_DATA:
+      return {
+        fetching: false,
+        users: action.users,
+      };
+    default:
+      return state;
+  }
+};
+
+const store3 = Redux.createStore(
+  asyncDataReducer,
+  Redux.applyMiddleware(ReduxThunk.default)
+);
+
+// ********** NEVER MUTATE STATE *****************
+
+const immutableReducer = (state = todos, action) => {
+  switch (action.type) {
+    case ADD_TO_DO:
+      // Don't mutate state here or the tests will fail
+      return [...state, action.todo];
+    default:
+      return state;
+  }
+};
+
+// Note: The spread syntax can be used multiple times in array composition like this,
+//       but it's important to note that it only makes a shallow copy of the array.
+
+// ***************** USING REDUX WITH REACT ************************
