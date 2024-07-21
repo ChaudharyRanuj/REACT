b250f624a8fad8e0072e72f098ce50903e200d33
Ranuj Chaudhary <ranujchoudhary@gmail.com>
2024-07-21 21:27:07 +0530

    react prop pattern compound component hoc component

---
+++ src/REACT NOTES/REACT PROJECT FOLDER STRUCTURE/react_folder_structure.js
@@ -0,0 +1,49 @@
+// INSIDE SRC
+
+    // pages (for routes and navigation)
+
+        /*
+        Homepage.jsx
+        Login.jsx
+        Register.jsx
+        */
+
+    // components (re usable)
+
+        // ui (subFolder)
+            /*
+            ListItem.jsx
+            Form.jsx
+            Modal.jsx
+            TextInput.jsx
+            */
+
+    // hooks
+
+        /*
+        useCountDown.jsx
+        useCurrentUser.jsx
+        useDebounce.jsx
+        */
+
+    // services
+    /*
+        // api (folder)
+            /*
+            apiRestaurant.jsx
+            */
+        // i18n
+
+    // state
+        // providers
+            /*
+            context
+            */
+
+    // utils
+        /*
+        helpers.jsx
+        formatting.jsx
+        */      
+     
+    // assets (images and files)
